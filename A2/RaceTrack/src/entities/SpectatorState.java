800a318dfe1c5ba6f684769b706b45d1b66fa2c3
Tiago Madeira <tiagomfmadeira@gmail.com>
2018-05-04 21:19:42 +0100

    documentation for the entity states

--- A2/RaceTrack/src/entities/SpectatorState.java
+++ A2/RaceTrack/src/entities/SpectatorState.java
@@ -5,15 +5,38 @@ package entities;
  */
 public enum SpectatorState
 {
+    /**
+     * Represents the state of the Spectator Waiting For a Race to start
+     */
     WAITING_FOR_A_RACE_TO_START("WFR"),
+    /**
+     * Represents the state of the Spectator Appraising The Horses
+     */
     APPRAISING_THE_HORSES("ATH"),
+    /**
+     * Represents the state of the Spectator Placing A Bet
+     */
     PLACING_A_BET("PAB"),
+    /**
+     * Represents the state of the Spectator Watching A Race
+     */
     WATCHING_A_RACE("WAR"),
+    /**
+     * Represents the state of the Spectator Collecting The Gains
+     */
     COLLECTING_THE_GAINS("CTG"),
+    /**
+     * Represents the state of the Spectator Celebrating
+     */
     CELEBRATING("C");
 
     private final String acronym;
 
+    /**
+     * Constructor
+     *
+     * @param acronym the acronym of the state
+     */
     SpectatorState(String acronym)
     {
 
--- A2/Stable/src/entities/HorseJockeyState.java
+++ A2/Stable/src/entities/HorseJockeyState.java
@@ -6,14 +6,34 @@ package entities;
  */
 public enum HorseJockeyState
 {
+    /**
+     * Represents the state of the Horse/Jockey At the Stable
+     */
     AT_THE_STABLE("ATS"),
+    /**
+     * Represents the state of the Horse/Jockey At The Paddock
+     */
     AT_THE_PADDOCK("ATP"),
+    /**
+     * Represents the state of the Horse/Jockey At the Start Line
+     */
     AT_THE_START_LINE("ASL"),
+    /**
+     * Represents the state of the Horse/Jockey Running
+     */
     RUNNING("R"),
+    /**
+     * Represents the state of the Horse/Jockey At The Finish line
+     */
     AT_THE_FINNISH_LINE("ATF");
 
     private final String acronym;
 
+    /**
+     * Constructor
+     *
+     * @param acronym the acronym of the state
+     */
     HorseJockeyState(String acronym)
     {
 
