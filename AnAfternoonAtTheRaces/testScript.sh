2466fe9de17a0eae15d9070fb19cf21810253bc8
Tiago Madeira <tiagomfmadeira@gmail.com>
2019-03-09 18:53:37 +0000

    Cleanup complete

--- AnAfternoonAtTheRaces/testScript.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#usage: testScrypt.sh <n_iterations>
-for ((i=1;i<=$1;i++)); do
-    java -classpath /home/silverio/SD/afternoon_races/AnAfternoonAtTheRaces/out/production/afternoon_races:/home/silverio/SD/genclass.jar main.MainDatatype
-done
